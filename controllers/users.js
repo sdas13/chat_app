dcca0f7263d3b5ffe58cdbfa4ac5ff2c875ae682
sdas13 <s.das001@yahoo.com>
2018-04-13 00:00:04 +0530

    added passport login authentication

--- controllers/users.js
+++ controllers/users.js
@@ -5,6 +5,7 @@ module.exports = function(_, passport){
     return {
         SetRouting: function(router){
            router.get('/',this.indexPage);
+           router.post('/',this.postlogin);        
            router.get('/signup',this.signupPage);
            router.post('/signup',this.postSignup);
            router.get('/home',this.homePage);
@@ -15,6 +16,11 @@ module.exports = function(_, passport){
         signupPage:function(req,res){
             return res.render('signup')
         },
+        postLogin:passport.authenticate('local.login',{
+            successRedirect:'/home',
+            failureRedirect:'/',
+            failureFlash:true
+        }),
         postSignup:passport.authenticate('local.signup',{
             successRedirect:'/home',
             failureRedirect:'/signup',
--- passport/passport-local.js
+++ passport/passport-local.js
@@ -41,3 +41,21 @@ passport.use('local.signup',new LocalStrategy({
     
 }));
 
+
+passport.use('local.login',new LocalStrategy({
+    usernameField:'email',
+    passwordField:'password',
+    passReqToCallback:true
+},(req,email,password,done)=>{
+    
+    User.findOne({'email':email},(err,user)=>{
+        if(err)
+            return done(err)
+        
+        if(!user || password!==user.password)
+            return done(null,false,req.flash('Either username or passport is incorrect'))
+        
+        return done(null,user);
+    })
+    
+}));
