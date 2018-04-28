c7fb43aa164f925f5283d935cfda7dff1daf7ef0
sdas13 <s.das001@yahoo.com>
2018-04-29 01:07:05 +0530

    protected route

--- routes/users.js
+++ routes/users.js
@@ -2,18 +2,51 @@ const express = require('express');
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
 const router = express.Router();
-
 const User = require('../models/user');
 
-router.post('/login',function (req,res) {
-    let user={id:1}
-    let token=jwt.sign({user},'secret_key');
-    res.json({token})
+router.post('/login', function (req, res) {
+    let user = {
+        id: 1
+    }
+    let token = jwt.sign({
+        user
+    }, 'secret_key');
+    res.json({
+        token
+    })
 })
 
-router.get('/api',function(req,res) {
+router.get('/protected', ensureToken, function (req, res) {
+    jwt.verify(req.token, 'secret_key', function (err, data) {
+        if (err)
+            res.sendStatus(403)
+        else
+            res.json({
+                text: 'This is protected',
+                data: data
+            })
+    })
+
+})
+
+function ensureToken(req, res, next) {
+    console.log(req.headers);
+
+    const bearerHeader = req.headers["authorization"];
+    if (typeof bearerHeader !== 'undefined') {
+        const bearer = bearerHeader.split(" ");
+        const bearerToken = bearer[1];
+        req.token = bearerToken;
+        next();
+    } else {
+        res.sendStatus(403);
+    }
+}
+
+
+router.get('/api', function (req, res) {
     res.json({
-        text:'my api'
+        text: 'my api'
     })
 })
 
