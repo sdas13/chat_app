b612022454440d59a89b55749b0382d640dff8ae
sdas13 <s.das001@yahoo.com>
2018-04-28 23:59:02 +0530

    jwt login

--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,19 @@ const router = express.Router();
 
 const User = require('../models/user');
 
+router.post('/login',function (req,res) {
+    let user={id:1}
+    let token=jwt.sign({user},'secret_key');
+    res.json({token})
+})
+
+router.get('/api',function(req,res) {
+    res.json({
+        text:'my api'
+    })
+})
+
+
 router.post('/register', function (req, res) {
     let user = new User();
 
