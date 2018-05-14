61b76a3a3c3270d6fc668b46a4ec1e9db86a6880
sdas13 <s.das001@yahoo.com>
2018-05-14 23:59:02 +0530

    create angular auth service

---
+++ angular-src/src/app/services/auth.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http:Http) { }
+}
